cfccd287f51867d2ee5c9c266c420b9d538a3411
yossiz16 <yossiz16@gmail.com>
2020-11-23 09:17:11 +0200

    Update ci.sh

--- images/ci.sh
+++ images/ci.sh
@@ -5,7 +5,7 @@ set sha=$(git rev-parse HEAD)
 for d in */ ; do
   if [ -f "${d}Dockerfile" ]; then
     echo -e "running docker build -t microcosm-${d%/}:latest ${d}${NC}"
-    docker build -q -t $sha -t http://645470188746.dkr.ecr.eu-west-1.amazonaws.com/$d:latest
-    docker push http://645470188746.dkr.ecr.eu-west-1.amazonaws.com/$d
+    echo docker build -q -t $sha -t http://645470188746.dkr.ecr.eu-west-1.amazonaws.com/$d:latest
+    echo docker push http://645470188746.dkr.ecr.eu-west-1.amazonaws.com/$d
   fi
 done
