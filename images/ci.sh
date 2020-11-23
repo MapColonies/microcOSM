12b66b1960b7aad0e5e9987b570e477a7a28602d
yossiz16 <yossiz16@gmail.com>
2020-11-23 13:57:33 +0200

    Update ci.sh

--- images/ci.sh
+++ images/ci.sh
@@ -2,8 +2,8 @@
 #!/bin/bash
 echo $PWD
 set -e
-SHA=$(git rev-parse HEAD)
-TAG=$(git tag)
+SHA=`git rev-parse HEAD`
+TAG=`git tag`
 ECR=http://645470188746.dkr.ecr.eu-west-1.amazonaws.com
 echo $TAG
 for d in */ ; do
