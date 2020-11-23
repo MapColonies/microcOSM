5f84a5464e9839a89f91333885480d958f2f35bb
yossiz16 <yossiz16@gmail.com>
2020-11-23 11:11:28 +0200

    Update ci.sh

--- images/ci.sh
+++ images/ci.sh
@@ -9,7 +9,7 @@ for d in */ ; do
     filename=$(basename -- "$d")
     foldername=${filename%.*}
     echo $filename
-    echo -e "running docker build -t microcosm-${foldername}:latest ${d}${NC}"
+    echo -e "running docker build -t microcosm-${foldername}:$TAG ${d}${NC}"
     echo docker build -q -t $SHA -t http://645470188746.dkr.ecr.eu-west-1.amazonaws.com/$foldername:$TAG
     echo docker push http://645470188746.dkr.ecr.eu-west-1.amazonaws.com/$foldername:$TAG
   fi
