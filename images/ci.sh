706e05b2ee051f81aa4bf9a5ed9884022adfa14a
yossiz16 <yossiz16@gmail.com>
2020-11-23 10:26:08 +0200

    Update ci.sh

--- images/ci.sh
+++ images/ci.sh
@@ -1,11 +1,15 @@
+
 #!/bin/bash
 set -e
-set sha=$(git rev-parse HEAD)
-
+SHA=$(git rev-parse HEAD)
+echo $SHA
 for d in */ ; do
   if [ -f "${d}Dockerfile" ]; then
-    echo -e "running docker build -t microcosm-${d%/}:latest ${d}${NC}"
-    echo docker build -q -t $sha -t http://645470188746.dkr.ecr.eu-west-1.amazonaws.com/$d:latest
-    echo docker push http://645470188746.dkr.ecr.eu-west-1.amazonaws.com/$d
+    filename=$(basename -- "$d")
+    foldername=${filename%.*}
+    echo $filename
+    echo -e "running docker build -t microcosm-${foldername}:latest ${d}${NC}"
+    echo docker build -q -t $SHA -t http://645470188746.dkr.ecr.eu-west-1.amazonaws.com/$foldername:latest
+    echo docker push http://645470188746.dkr.ecr.eu-west-1.amazonaws.com/$foldername:latest
   fi
 done
