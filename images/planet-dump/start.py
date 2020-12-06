69050b3e9544fa1a2fc9bc7ae27c522d505b000c
Daniel Hermon <danielhermonsync@gmail.com>
2020-12-06 10:39:31 +0200

    refactor(planet-dump): upgrade logger to v1.0.0 (#71)

--- images/planet-dump/start.py
+++ images/planet-dump/start.py
@@ -6,7 +6,8 @@ from datetime import datetime
 import time
 from croniter import croniter
 import pause
-from jsonlogger.logger import JSONLogger
+
+from MapColoniesJSONLogger.logger import generate_logger
 from osmeterium.run_command import run_command
 
 base_log_path = '/var/log'
@@ -22,6 +23,10 @@ dumps_storage_folder = os.environ.get('DUMP_STORAGE_FOLDER', '/mnt/dump')
 dump_file_prefix = os.environ.get('DUMP_FILE_PREFIX', 'dump')
 osmosis_omit_metadata = os.environ.get('OSMOSIS_OMIT_METADATA', 'true')
 
+log = None
+process_log = None
+app_name = 'planet-dump'
+osmosis = 'osmosis'
 
 def handle_osmosis_failure(exit_code):
     log.error('osmosis failed with exit code {}'.format(exit_code))
@@ -31,7 +36,7 @@ def handle_osmosis_failure(exit_code):
 def get_dump_file_name():
     iso_time = datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
     return '{0}-{1}.osm.pbf'.format(dump_file_prefix, iso_time)
-    
+
 
 def create_dump():
     file_name = get_dump_file_name()
@@ -42,7 +47,7 @@ def create_dump():
     start_time = time.perf_counter()
 
     run_command(create_dump_command, process_log.info,
-                process_log.error, handle_osmosis_failure, lambda: None)
+                process_log.info, handle_osmosis_failure, lambda: None)
 
     end_time = time.perf_counter()
 
@@ -67,11 +72,12 @@ if __name__ == '__main__':
     logs_folder = os.path.join(base_log_path, app_name)
     os.makedirs(logs_folder, exist_ok=True)
 
-    service_logs_path = os.path.join(logs_folder, "{}.log".format(app_name))
-    osmosis_logs_path = os.path.join(logs_folder, 'osmosis.log')
-    log = JSONLogger(
-        'main-debug', config={'handlers': {'file': {'filename': service_logs_path}}}, additional_fields={'service': app_name, 'description': 'service logs'})
-    process_log = JSONLogger(
-        'main-debug', config={'handlers': {'file': {'filename': osmosis_logs_path}}}, additional_fields={'service': app_name, 'description': 'osmosis logs'})
+    log_file_extention = '.log'
+    base_log_path = os.path.join('/var/log', app_name)
+    service_logs_path = os.path.join(base_log_path, app_name + log_file_extention)
+    mod_tile_log_path = os.path.join(base_log_path, osmosis + log_file_extention)
+    os.makedirs(base_log_path, exist_ok=True)
+    log = generate_logger(app_name, log_level='INFO', handlers=[{'type': 'rotating_file', 'path': service_logs_path},{ 'type': 'stream', 'output': 'stderr' }])
+    process_log = generate_logger(osmosis, log_level='INFO', handlers=[{'type': 'rotating_file', 'path': mod_tile_log_path}, { 'type': 'stream', 'output': 'stderr' }])
     log.info('planet dump container started')
-    main() 
+    main()
